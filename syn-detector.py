d215fc8c5912ecf0ca7da2dc2c3690276e607ca9
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-04 21:23:29 +0530

    Add method to show scanner info as a graph

--- syn-detector.py
+++ syn-detector.py
@@ -3,6 +3,7 @@
 from scapy.all import *
 import sys
 from collections import Counter
+import seaborn as sns
 
 
 def get_scanned_ports(pcap_fname, ip):
@@ -14,7 +15,19 @@ def get_scanned_ports(pcap_fname, ip):
     return list(set(portsScanned))
 
 
+def show_scanner_stats(scanner_dict):
+    sns.set_theme(style="darkgrid")
+    ax = sns.barplot(x=list(scanner_dict.keys()),
+                     y=list(scanner_dict.values()))
+    ax.set_xticklabels(list(scanner_dict.keys()), rotation=90)
+    ax.set_title("Port Scan Detection")
+    ax.set(xlabel='IP', ylabel='Number of ports scanned')
+    plt.subplots_adjust(bottom=0.2)
+    plt.show()
+
+
 def main(pcap_fname):
+    scanner_dict = {}
     syn_ip_list = []
     syn_ack_ip_list = []
 
@@ -30,12 +43,16 @@ def main(pcap_fname):
 
     for ip in syn_counts:
         if syn_counts[ip] > 3 * syn_ack_counts[ip]:
+            scanned_ports = get_scanned_ports(pcap_fname, ip)
+            scanner_dict[ip] = len(scanned_ports)
             print("Port scan conducted by IP:\n{}\nScanned ports:\n{}\n".format(
-                ip, ", ".join(str(x) for x in get_scanned_ports(pcap_fname, ip))))
+                ip, ", ".join(str(x) for x in scanned_ports)))
+
+    show_scanner_stats(scanner_dict)
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) <= 2:
         print('Use: python syn-detector.py file.pcap')
         sys.exit(-1)
     main(sys.argv[1])
